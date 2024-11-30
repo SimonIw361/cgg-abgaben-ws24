3a4fe19505b687bf1b648cb6d5a1f1792530eea9
Simon Iwanowski <siiw1213@bht-berlin.de>
2024-11-30 20:07:05 +0100

    Ebenen funktionieren,  Haus erstellt

--- src/cgg/a07/Ebene.java
+++ src/cgg/a07/Ebene.java
@@ -6,8 +6,6 @@ import cgg.a02.Hit;
 import cgg.a02.Ray;
 import cgg.a04.Material;
 import cgg.a05.Shape;
-import tools.Mat44;
-import tools.Vec2;
 import tools.Vec3;
 
 public class Ebene implements Shape{
@@ -15,9 +13,6 @@ public class Ebene implements Shape{
     private final double radius;
     private final double kantenlaenge;
     private Material material;
-
-    Mat44 transformationDrei;
-    Mat44 transformationDreiInvert;
 
     /**
      * Konstruktor fuer quadratische Ebene
@@ -39,13 +34,6 @@ public class Ebene implements Shape{
                 art = "quadratisch";
                 radius = 0;
                 kantenlaenge = s;
-                break;
-            case "dreieck":
-                art = "dreieck";
-                radius = 0;
-                kantenlaenge = s;
-                transformationDrei = rotate(vec3(0,0,1), 45);
-                transformationDreiInvert = invert(transformationDrei);
                 break;
             default:
                 throw new IllegalArgumentException("Diese Art der Ebene gibt es nicht. Bitte unbegrenzt, kreisrund oder quadratisch angeben");
@@ -74,26 +62,6 @@ public class Ebene implements Shape{
                 if(tPunkt.x() > kantenlaenge/2 || tPunkt.x() < -(kantenlaenge/2) || tPunkt.z() > kantenlaenge/2 || tPunkt.z() < -(kantenlaenge/2)){
                     return null;
                 }
-                break;
-            case "dreieck2":
-                if(tPunkt.x() > kantenlaenge/2 || tPunkt.x() < -(kantenlaenge/2) || tPunkt.z() > kantenlaenge/2 || tPunkt.z() < -(kantenlaenge/2) || tPunkt.x() < 0){
-                        return null;
-                }
-                break;
-            case "dreieck":
-                if(tPunkt.x() > kantenlaenge/2 || tPunkt.x() < -(kantenlaenge/2) || tPunkt.z() > kantenlaenge/2 || tPunkt.z() < -(kantenlaenge/2)){
-                    return null;
-                }
-                
-                Vec3 richtung = multiplyDirection(transformationDreiInvert, ray.getRichtung());
-                Vec3 punkt = multiplyPoint(transformationDreiInvert, ray.getX0());
-                t = (- punkt.y())/(richtung.y());
-                tPunkt = ray.gibStrahlPunkt(t);
-                if(tPunkt.x() < 0){
-                    //return null;
-                }
-                //t = (- ray.getX0().y())/(ray.getRichtung().y());
-                //tPunkt = ray.gibStrahlPunkt(t);
                 break;
         }
         
--- src/cgg/a07/Main.java
+++ src/cgg/a07/Main.java
@@ -27,6 +27,7 @@ public class Main {
     //Licht und Kamera erstellen
     ArrayList<Lichtquelle> licht = new ArrayList<>();
     licht.add(new Richtungslichtquelle(vec3(10,-10,10), white));
+    licht.add(new Richtungslichtquelle(vec3(-10,-10,10), white));
     Mat44 transformationKamera = multiply(move(vec3(0,-18,20.5)),rotate(vec3(1,0,0),22), rotate(vec3(0,1,0),-7));
     Lochkamera kamera = new Lochkamera(Math.PI/4, width, height, transformationKamera);
 
@@ -41,36 +42,32 @@ public class Main {
     //Array Liste mit allen Gruppen von Kugeln die in der Schleife erstellt werden
     ArrayList<Shape> gesichter = new ArrayList<>();
 
-    Kugel kopf = new Kugel(vec3(0,-0.2,-3), 1.4, rot);
-    Kugel kopf2 = new Kugel(vec3(0,-0.2,-3), 1.4, blau);
-    Kugel auge = new Kugel(vec3(0.42,-1.35,-2.29), 0.1, schwarz);
-    Kugel auge2 = new Kugel(vec3(-0.42,-1.35,-2.29), 0.1, schwarz);
-    Kugel nase = new Kugel(vec3(0,-1.04,-1.87), 0.1, orange);
-    Kugel mund1 = new Kugel(vec3(-0.09,-0.5,-1.7), 0.12, gelb);
-    Kugel mund2 = new Kugel(vec3(0.09,-0.5,-1.7), 0.12, gelb);
-    Kugel mund3 = new Kugel(vec3(-0.26,-0.58,-1.72), 0.11, gelb);
-    Kugel mund4 = new Kugel(vec3(0.26,-0.58,-1.72), 0.11, gelb);
-
-    Group augen = new Group(auge, auge2);
-    Group mundGesamt = new Group(mund1, mund2, mund3, mund4);
-    Group gesicht = new Group(kopf, augen, nase, mundGesamt);
-    Group gesicht2 = new Group(move(vec3(3,0,0)), kopf2, augen, nase, mundGesamt);
-    Group gruppe = new Group(gesicht, gesicht2);
+    Group wandVorne = new Group(multiply(move(vec3(0, 0, -10)), rotate(vec3(1,0,0), 90)), new Ebene("quadratisch", 3, new PhongMaterial(blue, white, 1000)));
+    Group wandSeiteLinks = new Group(multiply(move(vec3(-1.5, 0, -11.5)), rotate(vec3(0,0,1), 90)), new Ebene("quadratisch", 3, new PhongMaterial(blue, white, 1000)));
+    Group wandSeiteRechts = new Group(multiply(move(vec3(1.5, 0, -11.5)), rotate(vec3(0,0,1), -90)), new Ebene("quadratisch", 3, new PhongMaterial(blue, white, 1000)));
+    Group wandHinten = new Group(multiply(move(vec3(0, 0, -13)), rotate(vec3(1,0,0), 90)), new Ebene("quadratisch", 3, new PhongMaterial(blue, white, 1000)));
+    Group dachLinks = new Group(multiply(move(vec3(-1.07, -1.93, -11.5)), rotate(vec3(0,0,1), -45)), new Ebene("quadratisch", 3, new PhongMaterial(red, white, 1000)));
+    Group dachRechts = new Group(multiply(move(vec3(1.07, -1.93, -11.5)), rotate(vec3(0,0,1), 45)), new Ebene("quadratisch", 3, new PhongMaterial(red, white, 1000)));
+    Group fensterUnten1 = new Group(multiply(move(vec3(-0.75, -0.62, -9.99)), rotate(vec3(1,0,0), 90)), new Ebene("quadratisch", 0.8, new PhongMaterial(yellow, white, 1000)));
+    Group fensterUnten2 = new Group(multiply(move(vec3(0.75, 0.62, -9.99)), rotate(vec3(1,0,0), 90)), new Ebene("quadratisch", 0.8, new PhongMaterial(yellow, white, 1000)));
+    Group fensterUnten3 = new Group(multiply(move(vec3(-0.75, 0.62, -9.99)), rotate(vec3(1,0,0), 90)), new Ebene("quadratisch", 0.8, spiegel));
+    Group fensterUnten4 = new Group(multiply(move(vec3(0.75, -0.62, -9.99)), rotate(vec3(1,0,0), 90)), new Ebene("quadratisch", 0.8, spiegel));
+    
+    Group hausUnten = new Group(wandVorne, wandSeiteLinks, wandSeiteRechts, wandHinten, fensterUnten1, fensterUnten2, fensterUnten3, fensterUnten4);
+    Group hausOben = new Group(dachLinks, dachRechts);
+    Group haus = new Group(hausUnten, hausOben);
 
     for(int i=0; i < 1; i++){
       for(int j=0; j < 1; j++){
-        Group g = new Group(move(vec3(8*i, 0, -4 *j)), gruppe);
-        gesichter.add(g);
       }
     }
   
     Group groupSpiegel = new Group(new Kugel(vec3(-11,-4,-22), 6, spiegel));
     //Group ebene = new Group(multiply(move(0, 0, -10), rotate(vec3(1,0,0), 90)),new Ebene("kreisrund", 5.4, spiegel));
-    Group ebene2 = new Group(move(2, -2, -5), new Ebene("dreieck", 2.5, new PhongMaterial(blue, white, 1000)));
 
     Group alleGesichter = new Group(gesichter);
     Kugel hinterKugel = new Kugel(vec3(0,1001,-30), 1000, new DiffusStreuung(new ConstantColor(color(0.35)), new ConstantColor(white), new ConstantColor(color(1000.0))));//new PhongMaterial(color(0.35), white, 1000.0));
-    Group hintergrund = new Group(groupSpiegel, alleGesichter, ebene2);
+    Group hintergrund = new Group(groupSpiegel, alleGesichter, haus);
     Group welt = new Group(hintergrund);
     
     Image image = new Image(width, height);
